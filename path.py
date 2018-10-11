b6272554bb071dbd4d5bcd75b1ca57c1e43335b6
zunlihu <zunlihu@sjtu.edu.cn>
2018-10-11 11:23:38 +0800

    add new function

--- path.py
+++ path.py
@@ -5,3 +5,4 @@ if __name__ == '__main__':
     print(os.path.dirname(os.path.abspath('.')))
     print(os.path.abspath(os.path.join(os.path.dirname(__file__))))
     print(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+    print('ok')
